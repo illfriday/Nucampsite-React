0cf881651f726dd683f85c7b37babfc39de4eb74
illfriday <willprouty@gmail.com>
2021-03-13 13:04:45 -0800

    add About Component

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -5,6 +5,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
@@ -54,6 +55,7 @@ class Main extends Component {
             path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
           />
+          <Route path="/aboutus" render={() => <About partners={this.state.partners} />} />
           <Route path='/directory/:campsiteId' component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
